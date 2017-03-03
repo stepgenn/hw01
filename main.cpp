863c1db505e3c86d0a0ca0faa060f1e53f4cef85
stepgenn <s.Gennertova@seznam.cz>
2017-03-03 01:26:32 +0100

    encrypt, decrypt, hash wrote

--- main.cpp
+++ main.cpp
@@ -8,6 +8,43 @@
 #include "mbedtls/ctr_drbg.h"
 #include "mbedtls/sha512.h"
 
+bool loadKeyDecrypt(mbedtls_aes_context* aes, unsigned char key[32]) {
+	std::string keyFilePath;
+	std::cout << "Path - key file: ";
+	std::cin >> keyFilePath;
+	std::ifstream keyFile(keyFilePath, std::ios::in | std::ios::binary);
+	if (!keyFile.is_open()) {
+		std::cerr << "File " << keyFilePath << "wasn't found." << std::endl;
+		return false;
+	}
+
+	char keyBuffer[32];
+	keyFile.seekg(0,std::ios::beg);
+	keyFile.read(keyBuffer,32);
+	memcpy(key,reinterpret_cast<unsigned char *> (keyBuffer),32);
+	mbedtls_aes_setkey_dec( aes, key, 256 );
+	keyFile.close();
+	return true;
+}
+
+
+bool loadIvDecrypt(unsigned char iv[16]) {
+	std::string ivFilePath;
+	std::cout << "Path - initialization vector: ";
+	std::cin >> ivFilePath;
+	std::ifstream ivFile(ivFilePath, std::ios::in | std::ios::binary);
+	if (!ivFile.is_open()) {
+		std::cerr << "File " << ivFilePath << "wasn't found" << std::endl;
+		return false;
+	}
+
+	char ivBuffer[16];
+	ivFile.seekg(0,std::ios::beg);
+	ivFile.read(ivBuffer,16);
+	memcpy(iv,reinterpret_cast<unsigned char *> (ivBuffer),16);
+	return true;
+}
+
 long int sizeOfInputFile(std::ifstream* file) {
 	std::streampos begin;
 	std::streampos end;
@@ -30,8 +67,7 @@ bool generateAesKey(unsigned char key[32]) {
 	int ret;
 	mbedtls_entropy_init( &entropy );
 	mbedtls_ctr_drbg_init(&ctr_drbg);
-	if( ( ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy,
-	                                   (unsigned char *) pers, strlen( pers ) ) ) != 0 )
+	if( (ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (unsigned char *) pers, strlen(pers))) != 0 )
 	{
 		printf( " failed\n ! mbedtls_ctr_drbg_init returned -0x%04x\n", -ret );
 		return false;
@@ -45,34 +81,50 @@ bool generateAesKey(unsigned char key[32]) {
 	return true;
 }
 
-void getRandomVector(unsigned char iv[16]) {
+bool getRandomVector(unsigned char iv[16]) {
 	mbedtls_ctr_drbg_context ctr_drbg;
 	mbedtls_entropy_context entropy;
 	mbedtls_entropy_init( &entropy );
 	mbedtls_ctr_drbg_init(&ctr_drbg);
 	char *pers = "aes generate ivv";
-	mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy,
-	                       (unsigned char *) pers, strlen( pers ));
-	mbedtls_ctr_drbg_random( &ctr_drbg, iv, 16);
+	int ret;
+
+	if( (ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (unsigned char *) pers, strlen(pers))) != 0 )
+	{
+		printf( " failed\n ! mbedtls_ctr_drbg_init returned -0x%04x\n", -ret );
+		return false;
+	}
+	if( (ret = mbedtls_ctr_drbg_random( &ctr_drbg, iv, 16)) != 0 )
+	{
+		printf( " failed\n ! mbedtls_ctr_drbg_random returned -0x%04x\n", -ret );
+		return false;
+	}
+	return true;
 }
 
-//---------------Declare the variables needed for AES encryption.-------------
 
-bool encryptData(std::ifstream* inputFile, std::ofstream* outputFile) {
+
+void encryptData(std::ifstream* inputFile, std::ofstream* outputFile) {
 	mbedtls_aes_context aes;
 
 	unsigned char key[32];
-	generateAesKey(key);
+	if(!generateAesKey(key)) {
+		return;
+	}
 	mbedtls_aes_setkey_enc( &aes, key, 256 );
-	std::ofstream keyFile("outputs/encryptKey.key", std::ios::out | std::ios::binary | std::ios::trunc);
+	std::ofstream keyFile("outputs/encrypt.key", std::ios::out | std::ios::binary | std::ios::trunc);
 	keyFile.write(reinterpret_cast<char *> (key),32);
 	keyFile.close();
+	std::cout << "File outputs/encrypt.key was created." << std::endl;
 
 	unsigned char iv[16];
-	getRandomVector(iv);
-	std::ofstream ivFile("outputs/initzializationVector.key", std::ios::out | std::ios::binary | std::ios::trunc);
+	if(!getRandomVector(iv)) {
+		return;
+	}
+	std::ofstream ivFile("outputs/initializationVector", std::ios::out | std::ios::binary | std::ios::trunc);
 	ivFile.write(reinterpret_cast<char *> (iv),16);
 	ivFile.close();
+	std::cout << "File outputs/initializationVector was created." << std::endl;
 
 	long int inputLen = sizeOfInputFile(inputFile);
 	inputFile->seekg(0,std::ios::beg);
@@ -87,9 +139,6 @@ bool encryptData(std::ifstream* inputFile, std::ofstream* outputFile) {
 		memcpy(ivv,iv, 16);
 		unsigned char tmp[16];
 		mbedtls_aes_crypt_cbc( &aes, MBEDTLS_AES_ENCRYPT, 16, iv, input, output );
-		std::cout << "input: " <<reinterpret_cast<char *> (input) << "    output" << reinterpret_cast<char *>(output) << std::endl;
-		mbedtls_aes_crypt_cbc( &aes, MBEDTLS_AES_DECRYPT, 16, ivv, output, tmp );
-		std::cout << "input: " << reinterpret_cast<char *> (output) << "    output" << reinterpret_cast<char *> (tmp) << std::endl;
 		outputFile->write(reinterpret_cast<char *> (output),16);
 		inputLen = inputLen-16;
 	}
@@ -104,27 +153,21 @@ bool encryptData(std::ifstream* inputFile, std::ofstream* outputFile) {
 		mbedtls_aes_crypt_cbc( &aes, MBEDTLS_AES_ENCRYPT, 16, iv, input, output );
 		outputFile->write(reinterpret_cast<char *> (output),16);
 	}
-	return true;        //TODO musi byt bool?
+	//return true;        //TODO musi byt bool?
 }
 
 void decryptData(std::ifstream* inputFile){
 	mbedtls_aes_context aes;
 
 	unsigned char key[32];
-	char keybuffer[32];
-	std::ifstream keyFile("outputs/encryptKey.key", std::ios::in | std::ios::binary);
-	keyFile.seekg(0,std::ios::beg);
-	keyFile.read(keybuffer,32);
-	memcpy(key,reinterpret_cast<unsigned char *> (keybuffer),32);
-	mbedtls_aes_setkey_enc( &aes, key, 256 );
-
+	if(!loadKeyDecrypt(&aes,key)) {
+		return;
+	}
 
 	unsigned char iv[16];
-	char ivbuffer[16];
-	std::ifstream ivFile("outputs/initVector.iv", std::ios::in | std::ios::binary);
-	ivFile.seekg(0,std::ios::beg);
-	ivFile.read(ivbuffer,16);
-	memcpy(iv,reinterpret_cast<unsigned char *> (ivbuffer),16);
+	if(!loadIvDecrypt(iv)) {
+		return;
+	}
 
 	std::ofstream decryptedData("outputs/decryptData", std::ios::out | std::ios::binary | std::ios::trunc);
 
@@ -133,21 +176,22 @@ void decryptData(std::ifstream* inputFile){
 	char inputChar[16];
 	unsigned char output[16];
 	unsigned char input[16];
-	while (inputLen >= 16) {
+	while (inputLen > 16) {
 		inputFile->read(inputChar,16);
 		memcpy(input,reinterpret_cast<unsigned char *> (inputChar),16);
 		mbedtls_aes_crypt_cbc( &aes, MBEDTLS_AES_DECRYPT, 16, iv, input, output );
 			decryptedData.write(reinterpret_cast<char *> (output),16);
 			inputLen = inputLen-16;
 	}
-/*	inputFile->read(inputChar,inputLen);
-	memcpy(input,reinterpret_cast<unsigned char *> (inputChar),inputLen);
-	mbedtls_aes_crypt_cbc( &aes, MBEDTLS_AES_DECRYPT, inputLen, iv, input, output);
-	decryptedData.write(reinterpret_cast<char *> (output),inputLen);
- */
+
+	inputFile->read(inputChar,16);
+	memcpy(input,reinterpret_cast<unsigned char *> (inputChar),16);
+	mbedtls_aes_crypt_cbc( &aes, MBEDTLS_AES_DECRYPT, 16, iv, input, output );
+	unsigned int endData = output[15];
+	decryptedData.write(reinterpret_cast<char *> (output),(16-endData));
 }
 
-void countHas(std::ifstream* input, unsigned char* output) {
+void countHash(std::ifstream* input, unsigned char* output) {
 	long int inputLen = sizeOfInputFile(input);
 	input->seekg(0,std::ios::beg);
 	char inputBuffer[inputLen];
@@ -159,15 +203,15 @@ void countHas(std::ifstream* input, unsigned char* output) {
 
 void hashData(std::ifstream* input) {
 	unsigned char output[64];
-	countHas(input,output);
-	std::ofstream hashFile("outputs/dataHash.hash", std::ios::out | std::ios::binary | std::ios::trunc);
+	countHash(input,output);
+	std::ofstream hashFile("outputs/data.hash", std::ios::out | std::ios::binary | std::ios::trunc);
 	hashFile.write(reinterpret_cast<char *> (output),64);
 }
 
 void verifyHash(std::ifstream* input, unsigned char* origHash) {
 	std::cout << "Verifying hash: ";
 	unsigned char countedHash[64];
-	countHas(input,countedHash);
+	countHash(input,countedHash);
 
 	if(std::memcmp(countedHash,origHash,64)==0) {
 		std::cout << "OK." << std::endl;
@@ -183,25 +227,22 @@ void verifyHash(std::ifstream* input, unsigned char* origHash) {
 int main() {
 	std::string inputFilePath;
 
-	std::string outputFilePath;
+	std::cout << "Path - input file for encryption: ";
+	std::cin >> inputFilePath;
+	std::ifstream inputFile(inputFilePath, std::ios::in | std::ios::binary);
 
-	//std::cout << "Path to the input file:";
-	//std::cin >> inputFilePath;
-	//std::ifstream inputFile(inputFilePath, std::ios::in | std::ios::binary);
-	std::ifstream inputFile("input.txt", std::ios::in | std::ios::binary);
-
-	//std::cout << "Path to the output file:";
-	//std::cin >> outputFilePath;
 	std::ofstream outputFile("outputs/output.aes", std::ios::out | std::ios::binary | std::ios::trunc);
-	//todo some fails?
-	//if(!inputFile.is_open())
 
 	encryptData(&inputFile,&outputFile);
+	std::cout << "File outputs/output.aes was created." << std::endl;
 	hashData(&inputFile);
+	std::cout << "File outputs/data.hash was created." << std::endl;
 	outputFile.close();
-	std::ifstream dataDec("outputs/output.aes", std::ios::in | std::ios::binary);
+
+	std::cout << "Path - input file for the decryption: ";
+	std::cin >> inputFilePath;
+	std::ifstream dataDec(inputFilePath, std::ios::in | std::ios::binary);
+	std::cout << "File outputs/decryptData was created." << std::endl;
 	decryptData(&dataDec);
-
-
 	return 0;
 }
